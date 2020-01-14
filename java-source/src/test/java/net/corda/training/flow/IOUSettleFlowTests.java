9e9b1156a6f1e6e1ce867bc823909c230d7df097
tomonoriyoshino <47024762+tomonoriyoshino@users.noreply.github.com>
2020-01-14 16:53:19 +0900

    Update IOUSettleFlowTests.java

--- java-source/src/test/java/net/corda/training/flow/IOUSettleFlowTests.java
+++ java-source/src/test/java/net/corda/training/flow/IOUSettleFlowTests.java
@@ -39,7 +39,9 @@ import static net.corda.training.TestUtils.BOB;
 
 /**
  * Practical exercise instructions Flows part 3.
+ *実習の手順フローパート3。
  * Uncomment the unit tests and use the hints + unit test body to complete the FLows such that the unit tests pass.
+ *ユニットテストのコメントを外し、ヒント+ユニットテスト本体を使用して、ユニットテストがパスするようにFLowを完了します。
  */
 public class IOUSettleFlowTests{
 
@@ -98,16 +100,26 @@ public class IOUSettleFlowTests{
      * Task 1.
      * The first task is to grab the [IOUState] for the given [linearId] from the vault, assemble a transaction
      * and sign it.
+     *最初のタスクは、指定された[linearId]の[IOUState]をボールトから取得し、トランザクションをアセンブルして署名することです。
      * TODO: Grab the IOU for the given [linearId] from the vault, build and sign the settle transaction.
+     * TODO：ボールトから特定の[linearId]のIOUを取得し、決済トランザクションをビルドして署名します。
      * Hints:
      * - Use the code from the [IOUTransferFlow] to get the correct [IOUState] from the vault.
+     *-[IOUTransferFlow]のコードを使用して、ボールトから正しい[IOUState]を取得します。
      * - You will need to use the [Cash.generateSpend] functionality of the vault to add the cash states and cash command
      *   to your transaction. The API is quite simple. It takes a reference to a [ServiceHub], [TransactionBuilder], an [Amount],
      *   our Identity as a [PartyAndCertificate], the [Party] object for the recipient, and a set of the spending parties.
+     *-キャッシュ状態とキャッシュコマンドをトランザクションに追加するには、ボールトの[Cash.generateSpend]機能を使用する必要があります。 
+     *APIは非常に単純です。 [ServiceHub]、[TransactionBuilder]、[Amount]、[PartyAndCertificate]としてのアイデンティティ、
+     *受信者の[Party]オブジェクト、および支出パーティのセットへの参照を取ります。
      *   The function will mutate your builder by adding the states and commands.
+     *この関数は、ステートとコマンドを追加することにより、ビルダーを変更します。
      * - You then need to produce the output [IOUState] by using the [IOUState.pay] function.
+     *-[IOUState.pay]関数を使用して、出力[IOUState]を生成する必要があります。
      * - Add the input [IOUState] [StateAndRef] and the new output [IOUState] to the transaction.
+     *-入力[IOUState] [StateAndRef]および新しい出力[IOUState]をトランザクションに追加します。
      * - Sign the transaction and return it.
+     *-トランザクションに署名して返します。
      */
     @Test
     public void flowReturnsCorrectlyFormedPartiallySignedTransaction() throws Exception {
@@ -121,7 +133,9 @@ public class IOUSettleFlowTests{
 
         SignedTransaction settleResult = futureSettleResult.get();
         // Check the transaction is well formed...
+        //トランザクションが正しい形式であることを確認します...
         // One output IOUState, one input IOUState reference, input and output cash
+        // 1つの出力IOUState、1つの入力IOUState参照、入力および出力キャッシュ
         a.transaction(() -> {
             try {
                 LedgerTransaction ledgerTx = settleResult.toLedgerTransaction(a.getServices(), false);
@@ -138,12 +152,15 @@ public class IOUSettleFlowTests{
 
                 // Sum all the output cash. This is complicated as there may be multiple cash output states with not all of them
                 // being assigned to the lender.
+                //すべての出力現金を合計します。 これは複雑です。複数の現金出力状態があり、
+                //それらのすべてが貸し手に割り当てられているわけではないからです。
                 List<Cash.State> outputCash = ledgerTx.getOutputs().stream()
                         .map(state -> (Cash.State) state.getData())
                         .filter(state -> state.getOwner().getOwningKey().equals(b.getInfo().getLegalIdentities().get(0).getOwningKey()))
                         .collect(Collectors.toList());
 
                 // Sum the acceptable cash sent to the lender
+                //貸し手に送られた許容現金を合計します
                 Amount<Currency> outputCashSum = new Amount<>(0, inputIOU.amount.getToken());
                 for (Cash.State cash: outputCash) {
                     Amount<Currency> addCash = new Amount<>(cash.getAmount().getQuantity(), cash.getAmount().getToken().getProduct());
@@ -169,8 +186,11 @@ public class IOUSettleFlowTests{
     /**
      * Task 2.
      * Only the borrower should be running this flow for a particular IOU.
+     * 特定のIOUに対してこのフローを実行しているのは借り手だけです。
      * TODO: Grab the IOU for the given [linearId] from the vault and check the node running the flow is the borrower.
+     * TODO：ボールトから特定の[linearId]のIOUを取得し、フローを実行しているノードが借用者であることを確認します。
      * Hint: Use the data within the iou obtained from the vault to check the right node is running the flow.
+     *ヒント：ボールトから取得したiou内のデータを使用して、適切なノードがフローを実行していることを確認します。
      */
     @Test
     public void settleFlowCanOnlyBeRunByBorrower() throws Exception {
@@ -191,10 +211,14 @@ public class IOUSettleFlowTests{
     /**
      * Task 3.
      * The borrower must have at least SOME cash in the right currency to pay the lender.
+     *借り手は、貸し手に支払うために少なくとも適切な通貨でいくらかの現金を持っていなければなりません。
      * TODO: Add a check in the flow to ensure that the borrower has a balance of cash in the right currency.
+     * TODO：フローにチェックを追加して、借り手が適切な通貨の現金残高を持っていることを確認します。
      * Hint:
      * - Use [getCashBalance(getServiceHub(), (Currency) amount.getToken())].
+     *-[getCashBalance（getServiceHub（）,（Currency）amount.getToken（））]を使用します。
      * - Use an if statement to check there is cash in the right currency present.
+     *-if文を使用して、適切な通貨の現金が存在することを確認します。
      */
     @Test
     public void borrowerMustHaveCashInRightCurrency() throws Exception {
@@ -215,8 +239,11 @@ public class IOUSettleFlowTests{
     /**
      * Task 4.
      * The borrower must have enough cash in the right currency to pay the lender.
+     *借り手は、貸し手に支払うために適切な通貨で十分な現金を持っている必要があります。
      * TODO: Add a check in the flow to ensure that the borrower has enough cash to pay the lender.
+     * TODO：フローにチェックを追加して、借り手が貸し手を支払うのに十分な現金を持っていることを確認します。
      * Hint: Add another if statement similar to the one required above.
+     *ヒント：上記で必要なものと同様の別のifステートメントを追加します。
      */
     @Test
     public void borrowerMustHaveEnoughCashInRightCurrency() throws Exception {
@@ -236,7 +263,9 @@ public class IOUSettleFlowTests{
     /**
      * Task 5.
      * We need to get the transaction signed by the other party.
+     *取引相手の署名を取得する必要があります。
      * TODO: Use a subFlow call to [initiateFlow] and the [SignTransactionFlow] to get a signature from the lender.
+     * TODO：[initiateFlow]および[SignTransactionFlow]へのsubFlow呼び出しを使用して、貸し手から署名を取得します。
      */
     @Test
     public void flowReturnsTransactionSignedByBothParties() throws Exception {
@@ -257,7 +286,9 @@ public class IOUSettleFlowTests{
     /**
      * Task 6.
      * We need to get the transaction signed by the notary service
+     *公証サービスによって署名されたトランザクションを取得する必要があります
      * TODO: Use a subFlow call to the [FinalityFlow] to get a signature from the lender.
+     * TODO：[FinalityFlow]へのsubFlow呼び出しを使用して、貸し手から署名を取得します。
      */
     @Test
     public void flowReturnsCommittedTransaction() throws Exception {
